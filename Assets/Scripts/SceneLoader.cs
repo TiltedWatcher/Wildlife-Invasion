3fd7dde36261e18801814937ac68840221655f9e
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-02-25 03:49:48 +0100

    Added mainMenu, loadMainmenu after a few seconds

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour{
+
+    [SerializeField] int mainMenuIndex = 1;
     // Start is called before the first frame update
     void Start(){
         
@@ -11,5 +14,42 @@ public class SceneLoader : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         
+    }
+
+    public void loadScene(int sceneIndex) {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void loadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void reloadScene() {
+        loadScene(SceneManager.GetActiveScene().buildIndex);   
+    }
+
+
+    public void loadMainMenu() {
+        loadScene(mainMenuIndex);
+    }
+
+    public IEnumerator loadSceneWithDelay(float secondsDelay, int sceneIndex) {
+        yield return new WaitForSeconds(secondsDelay);
+        loadScene(sceneIndex);
+    }
+
+    public IEnumerator loadSceneWithDelay(float secondsDelay, string sceneName) {
+        yield return new WaitForSeconds(secondsDelay);
+        loadScene(sceneName);
+    }
+
+    public IEnumerator reloadSceneWithDelay(float secondsDelay) {
+        yield return new WaitForSeconds(secondsDelay);
+        loadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    
+    public IEnumerator loadMainMenu(float secondsDelay) {
+        yield return new WaitForSeconds(secondsDelay);
+        loadScene(mainMenuIndex);
     }
 }
--- Assets/Scripts/SplashScreen.cs
+++ Assets/Scripts/SplashScreen.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SplashScreen : MonoBehaviour{
+
+    [SerializeField] float splashScreenTimer = 3f;
     // Start is called before the first frame update
     void Start(){
-        
+        var sceneLoader = GetComponent<SceneLoader>();
+        sceneLoader.loadMainMenu(splashScreenTimer);
     }
 
     // Update is called once per frame
