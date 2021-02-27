fbdc119edc50c6d48454c3ce2b19a72841c603ba
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-02-27 21:32:04 +0100

    Walk animation for lizard modified, also moving lizard with walk

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour{
+
+    [SerializeField][Range(0f,10f)] float walkSpeedMultiplier = 1f;
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        transform.Translate(Vector2.left * Time.deltaTime * walkSpeedMultiplier);
+    }
+}
