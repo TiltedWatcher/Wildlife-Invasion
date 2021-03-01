93b003963747ffa9dc1c149885c144333e3713ee
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-03-01 03:52:24 +0100

    Defender Selection UI implemented

---
+++ Assets/Scripts/DefenderButton.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderButton : MonoBehaviour{
+
+    private void OnMouseDown() {
+        var buttons = FindObjectsOfType<DefenderButton>();
+
+        foreach (DefenderButton button in buttons) {
+            button.GetComponent<SpriteRenderer>().color = new Color32(45,45,45,255);
+        }
+        
+        GetComponent<SpriteRenderer>().color = Color.white;
+
+    }
+}
