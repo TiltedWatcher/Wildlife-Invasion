9b6086dfeea7ea9513bd0d9c2352b8b7adebc1ad
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-03-01 02:39:49 +0100

    spawn defenders on mouseclick

--- Assets/Scripts/DefenderSpawner.cs
+++ Assets/Scripts/DefenderSpawner.cs
@@ -3,7 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DefenderSpawner : MonoBehaviour{
+
+    [SerializeField] GameObject defender;
     private void OnMouseDown() {
-        Debug.Log("Mouse pressed");
+
+        SpawnDefender(GetSquareClicked());
     }
+
+
+    private void SpawnDefender(Vector2 spawnPos) {
+        GameObject newDefender = Instantiate(defender, spawnPos, Quaternion.identity) as GameObject;
+
+    }
+
+    private Vector2 GetSquareClicked() {
+        Vector2 clickPos = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+        return worldPos;
+    }
+
 }
