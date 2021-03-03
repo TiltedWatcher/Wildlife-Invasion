139bd8f671d73a93ed1dac0fb604917526b1aef9
TiltedWaechter <marcel.albsmeier@gmail.com>
2021-03-03 03:05:51 +0100

    adjusted spawn position of lizards

--- Assets/Scripts/DefendersShooter.cs
+++ Assets/Scripts/DefendersShooter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,38 @@ public class DefendersShooter : MonoBehaviour{
     [SerializeField] Projectile projectilePrefab;
     [SerializeField] GameObject projectileShooter;
     [SerializeField] float projectileSpeed;
+
+    private void Start() {
+        setLaneSpawner();
+    }
+
+    private void Update() {
+
+
+        if (isAttackerInLane()) {
+            Debug.Log("Shooty McShootface");
+            //TODO change animation to shoot
+        } else {
+            Debug.Log("Ladida do nothing");
+            //TODO change animation to idle
+        }
+    }
+
+    private bool isAttackerInLane() {
+        return true;
+        throw new NotImplementedException();
+    }
+
+
+    private void setLaneSpawner() {
+        AttackerSpawner[] spawnersArray = FindObjectsOfType<AttackerSpawner>();
+
+        foreach (AttackerSpawner spawner in spawnersArray) {
+        
+        }
+
+    }
+
 
     public void Fire( ) {
         var projectile = Instantiate(projectilePrefab, projectileShooter.transform.position, Quaternion.identity);
